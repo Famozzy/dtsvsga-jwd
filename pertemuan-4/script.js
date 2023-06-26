4abe1306b6c80b391c3be6f51c4b97cfcbd0bcbd
Faidil <faidilfamozzy@gmail.com>
2023-06-26 19:22:16 +0800

    add pertemuan-4

---
+++ pertemuan-4/script.js
@@ -0,0 +1,12 @@
+let namaLengkap = "Faidil Anwar";
+let namaPanggilan = "Faidil";
+let statusPekerjaan = "Mahasiswa";
+let webKampus = "https://wicida.ac.id";
+
+alert("Selamat datang " + namaPanggilan);
+
+
+document.write("<p>Halo, nama saya " + namaLengkap + "</p>");
+document.write("<p>Saya adalah seorang " + statusPekerjaan + "</p>");
+document.write("<p>Saya kuliah di <a href=" + webKampus + ">STMIK Widya Cipta Dharma</a></p>");
+
